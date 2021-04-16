2d8007903fb88f2621a79b2bdf2169dbcb21cfd9
Eduardo Nascimento Alves <316014@ptin.corppt.com>
2021-04-16 09:09:26 -0300

    ajustes na funcionalidade retirada

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -202,7 +202,21 @@ class Billing extends Component {
 
                 </View>
                 <View style={styles.container} >
-                    <View >
+                {
+                    this.props.retirada_loja == true ? (
+                        <View>
+                            <Text style={{
+                                fontSize: 15,
+                                fontWeight:'bold',
+                                textAlign:'center',
+                                marginBottom:5, 
+                                color:'#ff4d4d'
+                            }}>
+                                Atençao: O pedido deverá ser retirado na loja.
+                            </Text>
+                        </View> 
+                    ):(
+                        <View >
                         <Text style={{
                             fontSize: 15,
                             fontWeight: 'bold',
@@ -228,6 +242,9 @@ class Billing extends Component {
                             {this.props.usuario.ponto_referencia != '' && this.props.usuario.ponto_referencia != null ? '' + this.props.usuario.ponto_referencia : ''}
                         </Text>
                     </View>
+                    )
+                }
+                    
                     {this.props.show_loader == true ? (
                         <View
                             style={{
